4566a996ec39a61cbdbccda0c8b811e763a1596e
karthik <karthik.venkata@kanini.com>
2021-01-27 12:44:37 +0530

    Add project files.

---
+++ Hard/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium_q1
+{
+    class Program
+    {
+        public static void DisplayMenu()
+        {
+            Data.Line();
+            Console.WriteLine("Please enter the option\n1.Add an employee\n2.Modify an employee detail\n3.Print all employee's details\n4.Delete and print an employee detail\n5.Exit");
+            Data.Line();
+        }
+        
+
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; ; i++)
+            {
+                DisplayMenu();
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Data.AddEmployee();
+                        break;
+                    case 2:
+                        Data.EditEmployee();
+                        break;
+                    case 3:
+                        Data.PrintAllEmployees();
+                        break;
+                    case 4:
+                        Data.PrintAndDeleteEmployee();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("invalid input \nSelect again ");
+                        break;
+                           
+                }
+
+
+
+            }
+
+
+        }
+
+        
+    }
+}
